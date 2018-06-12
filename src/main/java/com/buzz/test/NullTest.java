ef39c5499b2f65c7453a10607e938d6d1e44de83
Lucas Ley <ayaaa168@163.com>
2018-06-12 23:25:32 +0800

    add interface design contract

--- src/main/java/com/buzz/test/NullTest.java
+++ src/main/java/com/buzz/test/NullTest.java
@@ -1,8 +1,15 @@
 package com.buzz.test;
 
+import com.buzz.util.JsonUtil;
+
+import java.util.Map;
+
 public class NullTest {
     public static void main(String[] args) {
         Object object = new Object();
         String str = (String) null;
+        String json = "{\"type\":\"worksDetail\",\"detailFlag\":\"0\",\"workId\":\"231312312\"}";
+        Map<String, String> result = JsonUtil.fromJson("{\"type\":\"worksDetail\",\"detailFlag\":\"0\",\"workId\":\"231312312\"}", Map.class);
+        System.out.println(result);
     }
 }
